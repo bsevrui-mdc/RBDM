d017e664de7b167765e3c070644e0600dc33e30b
Borja <bsevrui500@iesmarquesdecomares.org>
2024-10-30 09:34:05 +0100

    refactor structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <?php include("includes/head-tag-contents.php");?>
+    </head>
+    <body>
+        <?php include("includes/design-top.php");?>
+        <?php include("includes/navigation.php");?>
+        <main>
+            <!-- Aquí iría nuestro código -->
+        </main>
+        <?php include("includes/footer.php");?>
+    </body>
+</html>
