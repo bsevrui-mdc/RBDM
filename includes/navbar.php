f7998d52dd2e216c2b2a6d3f53381e6dc8a69aaa
RafaRoldan10 <roldancamposrafael@gmail.com>
2024-11-05 12:38:32 +0100

    cambio

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,36 +9,36 @@
             />
           </a>
           <div class="me-0 d-flex d-lg-none justify-content-center align-items-center">
-              <i class="fa-solid fa-magnifying-glass text-secondary mx-3"></i>
+              <i class="fa-solid fa-magnifying-glass  mx-3"></i>
               <button
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#mynavbar"
           >
-          <i class="fa-solid fa-bars text-secondary"></i>
+          <i class="fa-solid fa-bars "></i>
           </button>
           </div>
           
           <div class="collapse navbar-collapse" id="mynavbar">
             <ul class="navbar-nav me-auto">
               <li class="nav-item">
-                <a class="nav-link text-white mx-3" href="#">PELICULAS</a>
+                <a class="nav-link mx-3" href="#">PELICULAS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white mx-3" href="#">SERIES</a>
+                <a class="nav-link mx-3" href="#">SERIES</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white mx-3" href="#">GENEROS</a>
+                <a class="nav-link mx-3" href="#">GENEROS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white mx-3 d-lg-none">PERFIL</a>
+                <a class="nav-link mx-3 d-lg-none">PERFIL</a>
               </li>
             </ul>
           </div>
           <div class="ms-auto d-lg-block d-none">
-              <i class="fa-solid fa-magnifying-glass me-5 text-secondary"></i>
-              <i class="fa-solid fa-user me-5 text-secondary "></i>
+              <i class="fa-solid fa-magnifying-glass me-5"></i>
+              <i class="fa-solid fa-user me-5 "></i>
           </div>
           
         </div>
