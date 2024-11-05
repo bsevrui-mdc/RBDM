d5f297b722ba1e2e5cf32e1b88e89069cf75adb9
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-05 12:21:40 +0100

    update on style

--- contact.php
+++ contact.php
@@ -3,22 +3,28 @@
 <html lang="es">
     <head>
         <?php include("includes/head-tags.php"); ?>
-        <style>
-            div {
-                border: 1px solid white;
-            }
-        </style>
     </head>
     <body>
         <!-- Añadir luego la navbar -->
         <main class="container-fluid contact">
             <div class="row">
-                <div class="col-6 content">
+                <div class="col-12 col-lg-6 content">
                     <h3>Encuéntranos en Redes Sociales</h3>
                 </div>
-                <div class="col-6 content">
+                <div class="d-none d-lg-block col-lg-6 content">
                     <h3>¿Necesitas contacto más directo?</h3>
                     <h3>Mándanos un Email</h3>
+                    <form>
+                        <div class="mb-3">
+                            <label class="form-label">Correo Electrónico:</label>
+                            <input type="email" class="form-control" id="">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Tu Mensaje:</label>
+                            <input type="email" class="form-control" id="">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </form>
                 </div>
             </div>
         </main>
