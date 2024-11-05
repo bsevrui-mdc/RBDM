cbf568eca758745cb245ecdcc21b7ef537264bbf
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-05 12:27:36 +0100

    updated style

--- contact.php
+++ contact.php
@@ -9,11 +9,11 @@
         <main class="container-fluid contact">
             <div class="row">
                 <div class="col-12 col-lg-6 content">
-                    <h3>Encuéntranos en Redes Sociales</h3>
+                    <h3 class="text-center">Encuéntranos en Redes Sociales</h3>
                 </div>
                 <div class="d-none d-lg-block col-lg-6 content">
-                    <h3>¿Necesitas contacto más directo?</h3>
-                    <h3>Mándanos un Email</h3>
+                    <h3 class="text-center">¿Necesitas contacto más directo?</h3>
+                    <h3 class="text-center">Mándanos un Email</h3>
                     <form>
                         <div class="mb-3">
                             <label class="form-label">Correo Electrónico:</label>
