ffb3caf7861c14f1a6ce3d59f483b5d6a545f91f
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-05 11:13:05 +0100

    working on contact & style update

--- contact.php
+++ contact.php
@@ -3,13 +3,35 @@
 <html lang="es">
     <head>
         <?php include("includes/head-tags.php"); ?>
+        <style>
+            div {
+                border: 1px solid white;
+            }
+
+            .content {
+                border-radius: 15px;
+                background-color: rgba(255, 255, 255, 0.10);
+            }
+
+            .content h3 {
+                text-align: center;
+                font-family: cambria;
+            }
+        </style>
     </head>
     <body>
         <!-- Añadir luego la navbar -->
-        <main>
-            
+        <main class="container-fluid">
+            <div class="row">
+                <div class="col-6 content">
+                    <h3>Encuéntranos en Redes Sociales</h3>
+                </div>
+                <div class="col-6 content">
+                    <h3>¿Necesitas contacto más directo?</h3>
+                    <h3>Mándanos un Email</h3>
+                </div>
+            </div>
         </main>
-        <?php include("includes/footer.php"); ?>
         <!-- Añadir luego el footer -->
     </body>
 </html>
