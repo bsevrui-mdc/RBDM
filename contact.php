1efa6f1e7c57b62696435e646f2c91231204b4a8
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-06 09:24:06 +0100

    working on contact

--- contact.php
+++ contact.php
@@ -13,23 +13,23 @@
                         <div class="col-12">
                             <h3 class="text-center">Encuéntranos en Redes Sociales</h3>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 d-flex justify-content-center">
                             <img src="/assets/icons/tiktok.svg" alt="TikTok_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_TT</a>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 d-flex justify-content-center">
                             <img src="/assets/icons/instagram.svg" alt="instagram_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_IG</a>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 d-flex justify-content-center">
                             <img src="/assets/icons/twitter-x.svg" alt="x_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_X</a>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 d-flex justify-content-center">
                             <img src="/assets/icons/facebook.svg" alt="facebook_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_F</a>
                         </div>
-                        <div class="col-12">
+                        <div class="col-12 d-flex justify-content-center">
                             <img src="/assets/icons/youtube.svg" alt="youtube_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_YT</a>
                         </div>
@@ -39,8 +39,12 @@
                     <div class="row">
                         <div class="col-12">
                             <h3 class="text-center">¿Necesitas contacto más directo?</h3>
+                        </div>
+                        <div class="col-12">
                             <h3 class="text-center">Mándanos un Email</h3>
-                            <form>
+                        </div>
+                        <div class="col-12">
+                            <form action="" method="POST">
                                 <div class="mb-3">
                                     <label class="form-label">Correo Electrónico:</label>
                                     <input type="email" class="form-control" id="">
@@ -49,7 +53,9 @@
                                     <label class="form-label">Tu Mensaje:</label>
                                     <textarea class="form-control"></textarea>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                <div class="mb-3 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
                             </form>
                         </div>
                     </div>
