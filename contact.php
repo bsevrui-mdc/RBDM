82e39c9c31db80baa05a1f7ab6cd597b2fbe1536
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-06 09:04:53 +0100

    updated icons & working on contact

--- contact.php
+++ contact.php
@@ -15,40 +15,44 @@
                         </div>
                         <div class="col-12">
                             <img src="/assets/icons/tiktok.svg" alt="TikTok_logo">
-                            <a class="text-decoration-none">@rbdm_gcm_TT</a>
+                            <a class="text-decoration-none" href="">@rbdm_gcm_TT</a>
                         </div>
                         <div class="col-12">
                             <img src="/assets/icons/instagram.svg" alt="instagram_logo">
-                            <a class="text-decoration-none">@rbdm_gcm_IG</a>
+                            <a class="text-decoration-none" href="">@rbdm_gcm_IG</a>
                         </div>
                         <div class="col-12">
                             <img src="/assets/icons/twitter-x.svg" alt="x_logo">
-                            <a class="text-decoration-none">@rbdm_gcm_X</a>
+                            <a class="text-decoration-none" href="">@rbdm_gcm_X</a>
                         </div>
                         <div class="col-12">
                             <img src="/assets/icons/facebook.svg" alt="facebook_logo">
-                            <a class="text-decoration-none">@rbdm_gcm_F</a>
+                            <a class="text-decoration-none" href="">@rbdm_gcm_F</a>
                         </div>
                         <div class="col-12">
                             <img src="/assets/icons/youtube.svg" alt="youtube_logo">
-                            <a class="text-decoration-none">@rbdm_gcm_YT</a>
+                            <a class="text-decoration-none" href="">@rbdm_gcm_YT</a>
                         </div>
                     </div>
                 </div>
                 <div class="d-none d-lg-block col-lg-6 content">
-                    <h3 class="text-center">¿Necesitas contacto más directo?</h3>
-                    <h3 class="text-center">Mándanos un Email</h3>
-                    <form>
-                        <div class="mb-3">
-                            <label class="form-label">Correo Electrónico:</label>
-                            <input type="email" class="form-control" id="">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="text-center">¿Necesitas contacto más directo?</h3>
+                            <h3 class="text-center">Mándanos un Email</h3>
+                            <form>
+                                <div class="mb-3">
+                                    <label class="form-label">Correo Electrónico:</label>
+                                    <input type="email" class="form-control" id="">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Tu Mensaje:</label>
+                                    <textarea class="form-control"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                            </form>
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label">Tu Mensaje:</label>
-                            <textarea class="form-control"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Enviar</button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </main>
