1e3b733993fa7b83bdfbbcdb80e6673015a306de
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-11 10:28:49 +0100

    contact.php completed

--- contact.php
+++ contact.php
@@ -7,51 +7,51 @@
     <body>
         <?php include("includes/navbar.php"); ?>
         <main class="container-fluid contact">
-            <div class="row">
+            <div class="row my-4">
                 <div class="col-12 col-lg-6 content">
-                    <div class="row">
+                    <div class="row mx-2">
                         <div class="col-12">
-                            <h3 class="text-center">Encuéntranos en Redes Sociales</h3>
+                            <h3 class="text-center my-lg-4">Encuéntranos en Redes Sociales</h3>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center rrss">
+                        <div class="col-12 d-flex justify-content-center align-items-center py-3 rrss">
                             <img src="/assets/icons/tiktok.svg" class="icon" alt="TikTok_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_TT</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center rrss">
+                        <div class="col-12 d-flex justify-content-center align-items-center py-3 rrss">
                             <img src="/assets/icons/instagram.svg" class="icon" alt="instagram_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_IG</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center rrss">
+                        <div class="col-12 d-flex justify-content-center align-items-center py-3 rrss">
                             <img src="/assets/icons/twitter-x.svg" class="icon" alt="x_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_X</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center rrss">
+                        <div class="col-12 d-flex justify-content-center align-items-center py-3 rrss">
                             <img src="/assets/icons/facebook.svg" class="icon" alt="facebook_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_F</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center rrss">
+                        <div class="col-12 d-flex justify-content-center align-items-center py-3 rrss">
                             <img src="/assets/icons/youtube.svg" class="icon" alt="youtube_logo">
                             <a class="text-decoration-none" href="">@rbdm_gcm_YT</a>
                         </div>
                     </div>
                 </div>
                 <div class="d-none d-lg-block col-lg-6 content">
-                    <div class="row form">
+                    <div class="row mx-2">
                         <div class="col-12">
-                            <h3 class="text-center">¿Necesitas contacto más directo?</h3>
+                            <h3 class="text-center my-lg-4">¿Necesitas contacto más directo?</h3>
                         </div>
                         <div class="col-12">
                             <h3 class="text-center">Mándanos un Email</h3>
                         </div>
                         <div class="col-12">
-                            <form action="" method="POST">
+                            <form action="" method="POST" class="px-lg-4">
                                 <div class="mb-3">
                                     <label class="form-label">Correo Electrónico:</label>
-                                    <input type="email" class="form-control" id="">
+                                    <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Tu Mensaje:</label>
-                                    <textarea class="form-control"></textarea>
+                                    <textarea class="form-control" placeholder="Tu mensaje aquí"></textarea>
                                 </div>
                                 <div class="mb-3 d-flex justify-content-center">
                                     <button type="submit" class="btn btn-primary">Enviar</button>
