d5f725bc89b5a5be90d50f41e84e258d2d899a03
RafaRoldan10 <roldancamposrafael@gmail.com>
2024-11-11 10:34:12 +0100

    topPeliculas changed

--- topPeliculas.php
+++ topPeliculas.php
@@ -7,8 +7,86 @@
     <body>
         <?php include("includes/navbar.php"); ?>
         <main>
-            <div class="container">
-                
+            <div class="container-fluid">
+                <div class="row my-4">
+                    <div class="col-lg-2 justify-content-center text-center d-flex">Ranking</div>
+                    <div class="col-lg-2 justify-content-center text-center d-flex">Imagen</div>
+                    <div class="col-lg-4 justify-content-center text-center d-flex">Titulo</div>
+                    <div class="col-lg-2 justify-content-center text-center d-flex">Valoración</div>
+                    <div class="col-lg-2 justify-content-center text-center d-flex">Tu nota</div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-lg-2 align-items-center justify-content-center d-flex"><h1>1</h1></div>
+                    <div class="col-lg-2 justify-content-center"><img src="./assets/img/logo.png" alt="imagen" class="img-fluid"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-75">
+                            <div class="col"><h1>título de película 1</h1></div>
+                        </div>
+                        <div class="row flex-grow-1 h-25">
+                            <div class="col"><h3>TERROR</h3></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1><i class="fa-solid fa-star text-primary px-2"></i>10</h1>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1>9.45</h1>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-lg-2 align-items-center justify-content-center d-flex"><h1>1</h1></div>
+                    <div class="col-lg-2 justify-content-center"><img src="./assets/img/logo.png" alt="imagen" class="img-fluid"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-75">
+                            <div class="col"><h1>título de película 1</h1></div>
+                        </div>
+                        <div class="row flex-grow-1 h-25">
+                            <div class="col"><h3>TERROR</h3></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1><i class="fa-solid fa-star text-primary px-2"></i>10</h1>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1>9.45</h1>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-lg-2 align-items-center justify-content-center d-flex"><h1>1</h1></div>
+                    <div class="col-lg-2 justify-content-center"><img src="./assets/img/logo.png" alt="imagen" class="img-fluid"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-75">
+                            <div class="col"><h1>título de película 1</h1></div>
+                        </div>
+                        <div class="row flex-grow-1 h-25">
+                            <div class="col"><h3>TERROR</h3></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1><i class="fa-solid fa-star text-primary px-2"></i>10</h1>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1>9.45</h1>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col-lg-2 align-items-center justify-content-center d-flex"><h1>1</h1></div>
+                    <div class="col-lg-2 justify-content-center"><img src="./assets/img/logo.png" alt="imagen" class="img-fluid"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-75">
+                            <div class="col"><h1>título de película 1</h1></div>
+                        </div>
+                        <div class="row flex-grow-1 h-25">
+                            <div class="col"><h3>TERROR</h3></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1><i class="fa-solid fa-star text-primary px-2"></i>10</h1>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-flex">
+                        <h1>9.45</h1>
+                    </div>
+                </div>
             </div>
         </main>
         <?php include("includes/footer.php"); ?>
