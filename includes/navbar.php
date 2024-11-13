c70c086167cf9e482f06e669f5245fc3feaff69f
RafaRoldan10 <roldancamposrafael@gmail.com>
2024-11-13 09:14:33 +0100

    navbar changed

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,46 +1,44 @@
 <header>
-<nav class="navbar navbar-expand-lg bg-primary text-uppercase" id="mainNav">
-<div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img
-              src="/assets/img/logo.png"
-              alt="logo"
-              height="60"
-            />
-          </a>
-          <div class="me-0 d-flex d-lg-none justify-content-center align-items-center">
-              <i class="fa-solid fa-magnifying-glass mx-3"></i>
-              <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mynavbar"
-          >
+  <nav class="navbar navbar-expand-lg bg-primary text-uppercase" id="mainNav">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/assets/img/logo.png"
+          alt="logo"
+          height="60" />
+      </a>
+      <div class="me-0 d-flex d-lg-none justify-content-center align-items-center">
+        <i class="fa-solid fa-magnifying-glass mx-3"></i>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mynavbar">
           <i class="fa-solid fa-bars "></i>
-          </button>
-          </div>
-          
-          <div class="collapse navbar-collapse" id="mynavbar">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#"><h3>PELICULAS</h3></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><h3>SERIES</h3></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"><h3>GENEROS</h3></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link d-lg-none"><h3>PERFIL</h3></a>
-              </li>
-            </ul>
-          </div>
-          <div class="ms-auto d-lg-block d-none">
-              <i class="fa-solid fa-magnifying-glass me-5"></i>
-              <i class="fa-solid fa-user me-5 "></i>
-          </div>
-          
-        </div>
-</nav>
+        </button>
+      </div>
+
+      <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">PELICULAS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SERIES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GENEROS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-lg-none">PERFIL</a>
+          </li>
+        </ul>
+      </div>
+      <div class="ms-auto d-lg-block d-none">
+        <i class="fa-solid fa-magnifying-glass me-5"></i>
+        <i class="fa-solid fa-user me-5 "></i>
+      </div>
+
+    </div>
+  </nav>
 </header>
