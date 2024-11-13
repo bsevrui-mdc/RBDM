ad62a18f2a84c8e52692eeabce436b7592bd01c7
Darío <145323149+Dariocastillo94@users.noreply.github.com>
2024-11-14 00:30:39 +0100

    working on

--- admin.php
+++ admin.php
@@ -1,32 +1,134 @@
-<?php include("includes/a_config.php");?>
+<?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <?php include("includes/head-tags.php");?>
-    </head>
-    <body>
-        <?php include("includes/navbar.php"); ?>
-        <main>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col">
-                        <img src="https://placehold.co/100x100" alt="">
-                        <h1>admin</h1>
-                        <p>nombre</p>
-                        <p>apll1</p>
-                        <p>apll2</p>
-                        <form action="" method="post">
-                            <input type="submit" value="Peliculas">
-                            <input type="submit" value="Series">
-                            <input type="submit" value="Usuarios">
-                        </form>
-                    </div>
-                    <div class="col">
 
+<head>
+    <?php include("includes/head-tags.php"); ?>
+</head>
+
+<body>
+    <?php include("includes/navbar.php"); ?>
+    <main>
+        <div class="container-fluid">
+            <div class="row py-2">
+                <!--Parte izquierda-->
+                <div class="col-lg-2">
+                    <div class="row ">
+                        <div class="col-lg-12">
+                            <img src="https://placehold.co/100x100" alt="">
+                        </div>
+                        <div class="col-lg-12">
+                            <h1>admin</h1>
+                            <p>nombre</p>
+                            <p>apll1</p>
+                            <p>apll2</p>
+                        </div>
+                        <div class="col-lg">
+                            <form action="" method="post" class="d-flex flex-column">
+                                <input type="submit" class="btn btn-primary mb-2 rounded-pill" value="Peliculas">
+                                <input type="submit" class="btn btn-primary mb-2 rounded-pill" value="Series">
+                                <input type="submit" class="btn btn-primary mb-2 rounded-pill" value="Usuarios">
+                            </form>
+                        </div>
                     </div>
-                </div>   
+                </div>
+                <!--Parte derecha-->
+                <div class="col-lg-10">
+                    <div class="row">
+                        <div class="col">
+                            <form class="d-flex justify-content-end" role="search" action="/buscar" method="GET">
+                                <input class="form-control me-2 rounded-pill w-25" type="search" placeholder="Search"
+                                    aria-label="Search" name="query" required>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col d-flex justify-content-between">
+                            <h1>Lista de Peliculas</h1>
+                            <input type="button" class="btn btn-primary w-25" value="Añadir">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="table-responsive">
+                                <table class="table table-dark table-borderless">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>Nombre</th>
+                                            <th>Genero</th>
+                                            <th>Fecha</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                        <tr>
+                                            <td>img</td>
+                                            <td>Fallguys</td>
+                                            <td>Infantil</td>
+                                            <td>18-11-2000</td>
+                                            <td>iconos</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-        </main>
-        <?php include "includes/footer.php" ?>
-    </body>
+        </div>
+    </main>
+    <?php include "includes/footer.php" ?>
+</body>
+
 </html>
