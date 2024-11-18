78f284efaedbb7a78e16bcc5e1e5dd71b4c35975
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-18 09:10:53 +0100

    login completed

--- login.php
+++ login.php
@@ -8,18 +8,18 @@
         <?php include("includes/navbar.php"); ?>
         <main class="container-fluid login">
             <div class="row justify-content-center">
-                <div class="col-12 col-lg-6 content">
+                <div class="col-12 col-lg-6 my-lg-4 content">
                     <div class="row">
-                        <div class="col-12">
+                        <div class="col-12 py-4 py-lg-3">
                             <h3 class="text-center">Inicio de Sesión</h3>
                         </div>
                         <div class="col-12">
-                            <form action="" method="POST" class="">
+                            <form action="" method="POST" class="px-3 px-lg-5">
                                 <div class="mb-3 text-center">
                                     <label class="form-label">Correo Electrónico/Nombre de usuario:</label>
                                     <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
                                 </div>
-                                <div class="mb-3">
+                                <div class="mb-3 text-center">
                                     <label class="form-label">Contraseña:</label>
                                     <input type="password" class="form-control" id="">
                                 </div>
@@ -27,7 +27,9 @@
                                     <button type="submit" class="btn btn-primary">Enviar</button>
                                 </div>
                             </form>
-                            <a href="" class="text-decoration-none text-center">¿Has olvidado tu contraseña?</a>
+                        </div>
+                        <div class="col-12 text-center py-3">
+                            <a href="" class="text-decoration-none">¿Has olvidado tu contraseña?</a>
                         </div>
                     </div>
                 </div>
