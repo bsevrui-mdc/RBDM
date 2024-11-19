652cefbca6e56edbdfb71432ec296218882a6f50
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-19 13:45:12 +0100

    refactor contact.php & login.php

--- contact.php
+++ contact.php
@@ -7,50 +7,44 @@
     <body>
         <?php include("includes/navbar.php"); ?>
         <main class="container-fluid contact">
-            <div class="row my-4">
-                <div id="enlacesContacto" class="col-12 col-lg-6 content">
-                    <div class="row mx-2">
-                        <div class="col-12">
-                            <h3 class="text-center my-lg-4">Encuéntranos en Redes Sociales</h3>
-                        </div>
-                        <div class="col-12 d-flex justify-content-center py-3">
+            <div class="row">
+                <div id="enlacesContacto" class="col-lg-6">
+                    <div class="my-3 content">
+                        <h3 class="text-center pt-lg-4">Encuéntranos en Redes Sociales</h3>
+                        <div class="d-flex justify-content-center py-3">
                             <a class="text-decoration-none enlaces" href=""><i class="fa-brands fa-tiktok fa-3x mx-4"></i>@rbdm_gcm_TT</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center py-3">
+                        <div class="d-flex justify-content-center py-3">
                             <a class="text-decoration-none enlaces" href=""><i class="fa-brands fa-instagram fa-3x mx-4"></i>@rbdm_gcm_IG</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center py-3">
+                        <div class="d-flex justify-content-center py-3">
                             <a class="text-decoration-none enlaces" href=""><i class="fa-brands fa-x-twitter fa-3x mx-4"></i>@rbdm_gcm_X</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center py-3">
+                        <div class="d-flex justify-content-center py-3">
                             <a class="text-decoration-none enlaces" href=""><i class="fa-brands fa-facebook fa-3x mx-4"></i>@rbdm_gcm_F</a>
                         </div>
-                        <div class="col-12 d-flex justify-content-center py-3">
+                        <div class="d-flex justify-content-center py-3">
                             <a class="text-decoration-none enlaces" href=""><i class="fa-brands fa-youtube fa-3x mx-4"></i>@rbdm_gcm_YT</a>
                         </div>
                     </div>
                 </div>
-                <div id="formularioContacto" class="col-12 col-lg-6 mt-5 mt-lg-0 content">
-                    <div class="row mx-2">
-                        <div class="col-12">
-                            <h3 class="text-center mt-lg-4">¿Necesitas contacto más directo?</h3>
-                            <h3 class="text-center mb-lg-3">Mándanos un Email</h3>
-                        </div>
-                        <div class="col-12">
-                            <form action="" method="POST" class="px-lg-4">
-                                <div class="mb-3 text-center">
-                                    <label class="form-label">Correo Electrónico:</label>
-                                    <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
-                                </div>
-                                <div class="mb-3 text-center">
-                                    <label class="form-label">Tu Mensaje:</label>
-                                    <textarea class="form-control" placeholder="Tu mensaje aquí"></textarea>
-                                </div>
-                                <div class="mb-3 d-flex justify-content-center">
-                                    <button type="submit" class="btn btn-primary">Enviar</button>
-                                </div>
-                            </form>
-                        </div>
+                <div id="formularioContacto" class="col-lg-6 mt-2 mt-lg-0">
+                    <div class="my-3 content">
+                        <h3 class="text-center pt-lg-4">¿Necesitas contacto más directo?</h3>
+                        <h3 class="text-center pb-lg-3">Mándanos un Email</h3>
+                        <form action="" method="POST" class="px-lg-4">
+                            <div class="pb-3 text-center">
+                                <label class="form-label">Correo Electrónico:</label>
+                                <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
+                            </div>
+                            <div class="pb-3 text-center">
+                                <label class="form-label">Tu Mensaje:</label>
+                                <textarea class="form-control" placeholder="Tu mensaje aquí"></textarea>
+                            </div>
+                            <div class="pb-3 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- login.php
+++ login.php
@@ -9,26 +9,22 @@
         <main class="container-fluid login">
             <div class="row justify-content-center">
                 <div class="col-lg-6 my-lg-4 content">
-                    <div class="py-4 pb-lg-1 pt-lg-3">
-                        <h3 class="text-center">Inicio de Sesión</h3>
-                    </div>
-                    <div>
-                        <form action="" method="POST" class="px-3 px-lg-5">
-                            <div class="mb-3 text-center">
-                                <label class="form-label">Correo Electrónico:</label>
-                                <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
-                            </div>
-                            <div class="mb-3 text-center">
-                                <label class="form-label">Contraseña:</label>
-                                <input type="password" class="form-control" id="">
-                            </div>
-                            <div class="mb-3 d-flex justify-content-center">
-                                <button type="submit" class="btn btn-primary">Iniciar</button>
-                            </div>
-                        </form>
-                    </div>
+                    <h3 class="text-center py-4 pb-lg-1 pt-lg-3">Inicio de Sesión</h3>
+                    <form action="" method="POST" class="px-3 px-lg-5">
+                        <div class="mb-3 text-center">
+                            <label class="form-label">Correo Electrónico:</label>
+                            <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
+                        </div>
+                        <div class="mb-3 text-center">
+                            <label class="form-label">Contraseña:</label>
+                            <input type="password" class="form-control" id="">
+                        </div>
+                        <div class="mb-3 d-flex justify-content-center">
+                            <button type="submit" class="btn btn-primary">Iniciar</button>
+                        </div>
+                    </form>
                     <div class="text-center pb-3 pt-1">
-                        <a href="" class="text-decoration-none">¿Has olvidado tu contraseña?</a>
+                        <a href="" class="text-decoration-none text-center pb-3 pt-1">¿Has olvidado tu contraseña?</a>
                     </div>
                 </div>
             </div>
