cf9a441cef5ef215e53561c65e8853b93de903e1
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-19 13:13:38 +0100

    refactor login.php

--- login.php
+++ login.php
@@ -8,29 +8,27 @@
         <?php include("includes/navbar.php"); ?>
         <main class="container-fluid login">
             <div class="row justify-content-center">
-                <div class="col-12 col-lg-6 my-lg-4 content">
-                    <div class="row">
-                        <div class="col-12 py-4  pb-lg-1 pt-lg-3">
-                            <h3 class="text-center">Inicio de Sesión</h3>
-                        </div>
-                        <div class="col-12">
-                            <form action="" method="POST" class="px-3 px-lg-5">
-                                <div class="mb-3 text-center">
-                                    <label class="form-label">Correo Electrónico:</label>
-                                    <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
-                                </div>
-                                <div class="mb-3 text-center">
-                                    <label class="form-label">Contraseña:</label>
-                                    <input type="password" class="form-control" id="">
-                                </div>
-                                <div class="mb-3 d-flex justify-content-center">
-                                    <button type="submit" class="btn btn-primary">Iniciar</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-12 text-center pb-3 pt-1">
-                            <a href="" class="text-decoration-none">¿Has olvidado tu contraseña?</a>
-                        </div>
+                <div class="col-lg-6 my-lg-4 content">
+                    <div class="py-4 pb-lg-1 pt-lg-3">
+                        <h3 class="text-center">Inicio de Sesión</h3>
+                    </div>
+                    <div>
+                        <form action="" method="POST" class="px-3 px-lg-5">
+                            <div class="mb-3 text-center">
+                                <label class="form-label">Correo Electrónico:</label>
+                                <input type="email" class="form-control" placeholder="ejemplo@ejemplo.com" id="">
+                            </div>
+                            <div class="mb-3 text-center">
+                                <label class="form-label">Contraseña:</label>
+                                <input type="password" class="form-control" id="">
+                            </div>
+                            <div class="mb-3 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary">Iniciar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="text-center pb-3 pt-1">
+                        <a href="" class="text-decoration-none">¿Has olvidado tu contraseña?</a>
                     </div>
                 </div>
             </div>
