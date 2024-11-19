fff253be354b8f3841fe4b1ced297b3b832cd1a4
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-19 14:03:31 +0100

    refactor footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,53 +1,52 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="d-none d-lg-block col-lg-2">
-                <img src="/assets/img/logo.PNG" class="logo img-fluid" alt="logo">
-            </div>
-            <div class="col-12 col-lg-8">
-                <div class="row">
-                    <div class="col-6 col-lg enlaces">
-                        <a href="" class="text-decoration-none">Inicio</a>
-                        <a href="" class="text-decoration-none">Películas</a>
-                        <a href="" class="text-decoration-none">Series</a>
-                        <a href="" class="text-decoration-none">Géneros</a>
-                    </div>
-                    <div class="col-6 col-lg enlaces">
-                        <a href="/contact.php" class="text-decoration-none">Ayuda</a>
-                        <a href="/contact.php#formularioContacto" class="text-decoration-none">Contacto</a>
-                        <a href="/contact.php#enlacesContacto" class="text-decoration-none">Enlaces</a>
-                    </div>
-                    <div class="d-block d-lg-none"><hr></div>
-                    <div class="col-12 col-lg enlaces">
-                        <a href="/about.php#cuAbout" class="text-decoration-none">Condiciones de Uso</a>
-                        <a href="/about.php#ppAbout" class="text-decoration-none">Política de Privacidad</a>
-                        <a href="/about.php#pcAbout" class="text-decoration-none">Política de Cookies</a>
-                    </div>
+<footer class="footer container-fluid">
+    <div class="row">
+        <div class="d-none d-lg-flex justify-content-center col-lg-2">
+            <img src="/assets/img/logo.PNG" class="logo img-fluid" alt="logo">
+        </div>
+        <div class="col-lg-8">
+            <div class="row">
+                <div class="col-lg-4 text-center enlaces">
+                    <a href="" class="text-decoration-none">Inicio</a>
+                    <a href="" class="text-decoration-none">Películas</a>
+                    <a href="" class="text-decoration-none">Series</a>
+                    <a href="" class="text-decoration-none">Géneros</a>
+                </div>
+                <div class="col-6 col-lg-4 text-center enlaces">
+                    <a href="/contact.php" class="text-decoration-none">Ayuda</a>
+                    <a href="/contact.php#formularioContacto" class="text-decoration-none">Contacto</a>
+                    <a href="/contact.php#enlacesContacto" class="text-decoration-none">Enlaces</a>
+                </div>
+                <div class="col-6 col-lg-4 text-center enlaces">
+                    <a href="/about.php#cuAbout" class="text-decoration-none">Condiciones de Uso</a>
+                    <a href="/about.php#ppAbout" class="text-decoration-none">Política de Privacidad</a>
+                    <a href="/about.php#pcAbout" class="text-decoration-none">Política de Cookies</a>
                 </div>
             </div>
-            <div class="col-lg-2 py-5 py-lg-0">
-                <div class="row">
-                    <div class="col-3 col-lg-12"><img src="/assets/icons/amazon.svg" class="icon" alt=""></div>
-                    <div class="col-3 col-lg-12"><img src="/assets/icons/netflix.svg" class="icon" alt=""></div>
-                    <div class="col-3 col-lg-12"><img src="/assets/icons/hbo.svg" class="icon" alt=""></div>
-                    <div class="col-3 col-lg-12"><img src="/assets/icons/disney.svg" class="icon" alt=""></div>
+        </div>
+        <div class="col-lg-2 py-5 py-lg-0">
+            <div class="row">
+                <div class="col-3 col-lg-12 d-flex justify-content-center">
+                    <img src="/assets/icons/amazon.svg" class="icon" alt="Prime Video">
                 </div>
-            </div> 
-        </div>
-        <div class="row"> 
-            <div class="col-12 d-flex justify-content-center pb-4">
-                <a href=""><i class="fa-brands fa-tiktok fa-3x px-3"></i></a>
-                <a href=""><i class="fa-brands fa-instagram fa-3x px-3"></i></a>
-                <a href=""><i class="fa-brands fa-x-twitter fa-3x px-3"></i></a>
-                <a href=""><i class="fa-brands fa-facebook fa-3x px-3"></i></a>
-                <a href=""><i class="fa-brands fa-youtube fa-3x px-3"></i></a>
-                <a href=""><i class="fa-brands fa-reddit fa-3x px-3"></i></a>
+                <div class="col-3 col-lg-12 d-flex justify-content-center">
+                    <img src="/assets/icons/netflix.svg" class="icon" alt="Netflix">
+                </div>
+                <div class="col-3 col-lg-12 d-flex justify-content-center">
+                    <img src="/assets/icons/hbo.svg" class="icon" alt="Max/HBO">
+                </div>
+                <div class="col-3 col-lg-12 d-flex justify-content-center">
+                    <img src="/assets/icons/disney.svg" class="icon" alt="Disney+">
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <p class="text-center">© 1990-2024&nbsp;by rbdm.com, Inc.</p>
-            </div>
+        <div class="d-flex justify-content-center pb-4">
+            <a href=""><i class="fa-brands fa-tiktok fa-3x px-3"></i></a>
+            <a href=""><i class="fa-brands fa-instagram fa-3x px-3"></i></a>
+            <a href=""><i class="fa-brands fa-x-twitter fa-3x px-3"></i></a>
+            <a href=""><i class="fa-brands fa-facebook fa-3x px-3"></i></a>
+            <a href=""><i class="fa-brands fa-youtube fa-3x px-3"></i></a>
+            <a href=""><i class="fa-brands fa-reddit fa-3x px-3"></i></a>
         </div>
+        <p class="text-center">© 1990-2024&nbsp;by rbdm.com, Inc.</p>
     </div>
 </footer>
