5b9c49a0c50b4d94f3e7aa18c6f10746d8324200
Darío <145323149+Dariocastillo94@users.noreply.github.com>
2024-11-23 20:03:12 +0100

    working on

--- admin.php
+++ admin.php
@@ -14,21 +14,29 @@
                 <!--Parte izquierda-->
                 <div class="col-lg-2">
                     <div class="row text-center">
-                        <div class="col-6 col-lg-12 ">
+                        <div class="col-3 col-lg-12">
                             <img src="https://placehold.co/150x150" alt="" class="imgAdmin">
                         </div>
-                        <div class="col-6 col-lg-12">
+                        <div class="col-9 d-lg-none d-sm-block align-content-center">
+                            <div class="row justify-content-around">
+                                <div class="col-auto">
+                                    <i class="fa-solid fa-film"></i>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fa-solid fa-tv"></i>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fa-solid fa-user"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 d-none d-lg-block">
                             <div class="display-3 nameAdmin">admin</div>
                             <p>nombre</p>
                             <p>apll1</p>
                             <p>apll2</p>
                         </div>
-                        <div class="col-6 col-lg-12 d-none">
-                            <i class="fa-solid fa-film"></i>
-                            <i class="fa-solid fa-tv"></i>
-                            <i class="fa-solid fa-user"></i>
-                        </div>
-                        <div class="col-lg">
+                        <div class="col-lg d-none d-lg-block">
                             <form action="" method="post" class="d-flex flex-column">
                                 <input type="submit" class="btn btn-primary mb-2 rounded-pill" value="Peliculas">
                                 <input type="submit" class="btn btn-primary mb-2 rounded-pill" value="Series">
@@ -40,7 +48,7 @@
                 <!--Parte derecha-->
                 <div class="col-lg-10">
                     <div class="row py-3">
-                        <div class="col-md-4 ms-auto">
+                        <div class="col-md-4 ms-auto d-none d-lg-block">
                             <div class="input-group">
                                 <input class="form-control py-2 border-right-0 border" type="search"
                                     placeholder="Buscar" id="example-search-input">
@@ -50,8 +58,8 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row py-3">
-                        <div class="col d-flex justify-content-between">
+                    <div class="row py-2">
+                        <div class="col  textList">
                             <h1>Lista de Peliculas</h1>
                             <input type="button" class="btn btn-primary w-25" value="Añadir">
                         </div>
