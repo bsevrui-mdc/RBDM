d56db977f9738c96cbcf52b75ace774b272286e3
MarianoRoldan47 <118228181+MarianoRoldan47@users.noreply.github.com>
2024-11-25 10:39:27 +0100

    update usuario.php

--- usuario.php
+++ usuario.php
@@ -18,21 +18,21 @@
         </div>
 
         <div class="container-fluid p-5 datos-usuario">
-            <div class="row bg-info p-5 rounded-3">
-                <div class="col-12 col-lg-4 foto-perfil align-content-center">
-                    <img src="https://placehold.co/320x320" class="img-fluid">
+            <div class="row bg-info p-lg-5 rounded-3">
+                <div class="col-12 col-lg-4 foto-perfil align-content-center mx-auto">
+                    <img src="https://placehold.co/320x320" class="img-fluid rounded-3">
                 </div>
                 <div class="col-12 col-lg align-content-center">
                     <div class="row h-100 align-content-around gap-4 campos text-dark p-3">
-                        <div class="col-12 bg-secondary campo p-2 rounded-4">
+                        <div class="col-12 bg-secondary campo p-lg-2 rounded-3">
                             <div class="fw-bold d-inline">Usuario:</div>
                             <div class="d-inline">VaMa_18</div>
                         </div>
-                        <div class="col-12 bg-secondary campo p-2 rounded-4 ">
+                        <div class="col-12 bg-secondary campo p-lg-2 rounded-3 ">
                             <div class="fw-bold d-inline">Nombre:</div>
                             <div class="d-inline">Ángeles Ruiz Siendones</div>
                         </div>
-                        <div class="col-12 bg-secondary campo p-2 rounded-4">
+                        <div class="col-12 bg-secondary campo p-lg-2 rounded-3">
                             <div class="fw-bold d-inline">Fecha de unión:</div>
                             <div class="d-inline">8-10-2024</div>
                         </div>
@@ -50,107 +50,103 @@
                 <div class="col-2 text-center">
                     <h3>Imagen</h3>
                 </div>
-                <div class="col-8 text-center">
-                    <h3>titulo</h3>
+                <div class="col text-center">
+                    <h3 class="m-0">titulo</h3>
                 </div>
-                <div class="col-2 text-center">
-                    <h3>Valoracion</h3>
+                <div class="col-3 text-center">
+                    <h3 class="m-0">Valoracion</h3>
                 </div>
             </div>
             <div class="row top">
-                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1080" alt="imagen"
-                        class="img-fluid"></div>
+                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1200" alt="imagen" class="img-fluid"></div>
                 <div class="col-lg d-flex flex-column">
                     <div class="row d-flex align-items-center h-75">
-                        <div class="col text-center text-lg-start">
-                            <h2>título de película 4</h2>
+                        <div class="col text-center text-lg-start py-2 py-lg-0">
+                            <h2>título de serie 2</h2>
                         </div>
                     </div>
-                    <div class="row h-25 align-content-center">
-                        <div class="col-8 bordeGenero m-0">
-                            <h3>TERROR</h3>
+                    <div class="row">
+                        <div class="col-7 bordeGenero  align-items-center my-auto">
+                            <h3 class="m-0">COMEDIA</h3>
                         </div>
-                        <div class="col-4 bordeNota d-flex justify-content-end d-lg-none m-0">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
-                </div>
-            </div>
-
-            <div class="row top">
-                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1080" alt="imagen"
-                        class="img-fluid"></div>
-                <div class="col-lg d-flex flex-column">
-                    <div class="row d-flex align-items-center h-75">
-                        <div class="col text-center text-lg-start">
-                            <h2>título de película 4</h2>
-                        </div>
-                    </div>
-                    <div class="row h-25  align-content-center">
-                        <div class="col-8 bordeGenero m-0">
-                            <h3>TERROR</h3>
-                        </div>
-                        <div class="col-4 bordeNota d-flex justify-content-end d-lg-none m-0">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
+                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                <div class="col-lg-3 justify-content-center align-items-center border-start d-lg-flex d-none">
+                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                 </div>
             </div>
 
             <div class="row top">
-                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1080" alt="imagen"
-                        class="img-fluid"></div>
+                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1200" alt="imagen" class="img-fluid"></div>
                 <div class="col-lg d-flex flex-column">
                     <div class="row d-flex align-items-center h-75">
-                        <div class="col text-center text-lg-start">
-                            <h2>título de película 4</h2>
+                        <div class="col text-center text-lg-start py-2 py-lg-0">
+                            <h2>título de serie 2</h2>
                         </div>
                     </div>
-                    <div class="row h-25  align-content-center">
-                        <div class="col-8 bordeGenero m-0">
-                            <h3>TERROR</h3>
+                    <div class="row">
+                        <div class="col-7 bordeGenero  align-items-center my-auto">
+                            <h3 class="m-0">COMEDIA</h3>
                         </div>
-                        <div class="col-4 bordeNota d-flex justify-content-end d-lg-none m-0">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
+                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                <div class="col-lg-3 justify-content-center align-items-center border-start d-lg-flex d-none">
+                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                 </div>
             </div>
 
             <div class="row top">
-                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1080" alt="imagen"
-                        class="img-fluid"></div>
+                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1200" alt="imagen" class="img-fluid"></div>
                 <div class="col-lg d-flex flex-column">
                     <div class="row d-flex align-items-center h-75">
-                        <div class="col text-center text-lg-start">
-                            <h2>título de película 4</h2>
+                        <div class="col text-center text-lg-start py-2 py-lg-0">
+                            <h2>título de serie 2</h2>
                         </div>
                     </div>
-                    <div class="row h-25 align-content-center">
-                        <div class="col-8 bordeGenero m-0">
-                            <h3>TERROR</h3>
+                    <div class="row">
+                        <div class="col-7 bordeGenero  align-items-center my-auto">
+                            <h3 class="m-0">COMEDIA</h3>
                         </div>
-                        <div class="col-4 bordeNota d-flex justify-content-end d-lg-none m-0">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
+                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.05</div>
+                <div class="col-lg-3 justify-content-center align-items-center border-start d-lg-flex d-none">
+                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
+                </div>
+            </div>
+
+            <div class="row top">
+                <div class="col-lg-2 contenedorImagen"><img src="https://placehold.co/1920x1200" alt="imagen" class="img-fluid"></div>
+                <div class="col-lg d-flex flex-column">
+                    <div class="row d-flex align-items-center h-75">
+                        <div class="col text-center text-lg-start py-2 py-lg-0">
+                            <h2>título de serie 2</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-7 bordeGenero  align-items-center my-auto">
+                            <h3 class="m-0">COMEDIA</h3>
+                        </div>
+                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
+                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 justify-content-center align-items-center border-start d-lg-flex d-none">
+                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
                 </div>
             </div>
         </div>
     </main>
-    <?php  // include "includes/footer.php" ?>
+    <?php include "includes/footer.php" ?>
 </body>
 
 </html>
