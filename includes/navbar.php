b163d124eae3767f9b9505505d8703abc3fa69a1
Borja <bsevrui500@iesmarquesdecomares.org>
2024-11-29 17:24:49 +0100

    fixes on navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,56 +1,42 @@
-<header>
-  <nav class="navbar navbar-expand-lg bg-primary text-uppercase" id="mainNav">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">
-        <img src="/assets/img/logo.png" alt="logo" height="60">
-      </a>
-      <div class="me-0 d-flex d-lg-none justify-content-center align-items-center">
-        <input class="me-2 rounded-2 px-2 w-75 d-none d-lg-block" placeholder="Search..." type="text" name="buscador">
-        <i class="fa-solid fa-magnifying-glass mx-3"></i>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-          <i class="fa-solid fa-bars "></i>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id="mynavbar">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="topPeliculas.php">Películas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="topSeries.php">Series</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="eleccionGenero.php">Géneros</a>
-          </li>
-          <li class="nav-item dropdown d-lg-none">
-            <a class="nav-link dropdown-toggle" href="#" id="usuarioDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              Usuario
-            </a>
-            <ul class="dropdown-menu bg-secondary dropdown-menu-end" aria-labelledby="usuarioDropdown">
-              <li><a class="dropdown-item item" href="usuario.php">Perfil</a></li>
-              <li><a class="dropdown-item item" href="admin.php">Administración</a></li>
-              <hr>
-              <li><a class="dropdown-item item" href="login.php">Log Out <i class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
-            </ul>
-          </li>
+<nav class="container-fluid navbar navbar-expand-lg bg-primary text-uppercase px-3" id="mainNav">
+  <a class="navbar-brand" href="index.php">
+    <img src="/assets/img/logo.png" alt="logo" height="60">
+  </a>
+  <!-- Navbar <lg -->
+  <div class="me-0 d-flex d-lg-none justify-content-center align-items-center">
+    <a href="" class="mx-3"><i class="fa-solid fa-magnifying-glass"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <i class="fa-solid fa-bars "></i>
+    </button>
+  </div>
+  <div class="collapse navbar-collapse" id="mynavbar">
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item"><a class="nav-link" href="topPeliculas.php">Películas</a></li>
+      <li class="nav-item"><a class="nav-link" href="topSeries.php">Series</a></li>
+      <li class="nav-item"><a class="nav-link" href="eleccionGenero.php">Géneros</a></li>
+      <li class="d-lg-none nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="usuarioDropdown" data-bs-toggle="dropdown" aria-expanded="false">Usuario</a>
+        <ul class="dropdown-menu bg-secondary dropdown-menu-end menuDropdown" aria-labelledby="usuarioDropdown">
+          <li><a class="dropdown-item item" href="usuario.php">Perfil</a></li>
+          <li><a class="dropdown-item item" href="admin.php">Administración</a></li>
+          <hr>
+          <li><a class="dropdown-item item" href="login.php">Log Out <i class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
         </ul>
-      </div>
-
-      <div class="ms-auto d-lg-flex d-none iconos me-5">
-        <input class="me-2 rounded-2 px-2" placeholder="Search..." type="text" name="buscador">
-        <i class="fa-solid fa-magnifying-glass me-5"></i>
-        <div class="dropdown">
-          <a href="#" class="d-inline-block text-white" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fa-solid fa-user"></i>
-          </a>
-          <ul class="dropdown-menu bg-secondary dropdown-menu-end">
-            <li><a class="dropdown-item" href="usuario.php">Perfil</a></li>
-            <li><a class="dropdown-item" href="admin.php">Administracion</a></li>
-            <hr>
-            <li><a class="dropdown-item" href="login.php">Log Out <i class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
-          </ul>
-        </div>
-      </div>
+      </li>
+    </ul>
+  </div>
+  <!-- Navbar >=lg -->
+  <div class="d-none d-lg-flex me-5 align-items-center ms-auto me-5 iconos">
+    <input class="me-2" type="text" placeholder="Buscar..." name="buscador">
+    <a href="" class="ms-3 me-4"><i class="fa-solid fa-magnifying-glass"></i></a>
+    <div class="dropdown">
+      <a href="#" class="d-inline-block text-white" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-user"></i></a>
+      <ul class="dropdown-menu bg-secondary dropdown-menu-end menuDropdown">
+        <li><a class="dropdown-item" href="usuario.php">Perfil</a></li>
+        <li><a class="dropdown-item" href="admin.php">Administracion</a></li>
+        <hr>
+        <li><a class="dropdown-item" href="login.php">Log Out <i class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
+      </ul>
     </div>
-  </nav>
-</header>
+  </div>
+</nav>
