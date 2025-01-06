81882f7efee3695fc62beea745a23910393f7be7
Borja <bsevrui500@iesmarquesdecomares.org>
2025-01-06 19:50:35 +0100

    working on memory game

--- js/juegoMemoria.js
+++ js/juegoMemoria.js
@@ -1,1 +1,30 @@
-console.log("prueba");
+const CARDS = document.querySelectorAll(".carta");
+var carta1, carta2;
+
+function matchCards(img1, img2) {
+    if (img1 === img2) {
+        return console.log("cards matched");
+    } else {
+        console.log("cards don't match");
+        carta1.classList.add("shake");
+        carta2.classList.add("shake");
+    }
+}
+
+function flipCard(e) {
+    let carta = e.target;
+    if (carta !== carta1) {
+        carta.classList.add("flip");
+        if (!carta1) {
+            return carta1 = carta;
+        }
+        carta2 = carta;
+        let carta1Img = carta1.querySelector("img").src;
+        let carta2Img = carta2.querySelector("img").src;
+        matchCards(carta1Img, carta2Img);
+    }
+}
+
+CARDS.forEach(carta => {
+    carta.addEventListener("click", flipCard);
+});
