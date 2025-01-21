30a717651f17495bd5adeeff13e10f28c0ef02c1
Borja <bsevrui500@iesmarquesdecomares.org>
2025-01-21 20:01:01 +0100

    update on login

--- login.php
+++ login.php
@@ -44,11 +44,11 @@
                     <form action="login.php" method="POST" class="px-3 px-lg-5">
                         <div class="mb-3 text-center">
                             <label class="form-label">Correo Electrónico:</label>
-                            <input type="email" name="email" class="form-control" placeholder="ejemplo@ejemplo.com">
+                            <input type="email" name="email" class="form-control" placeholder="ejemplo@ejemplo.com" required>
                         </div>
                         <div class="mb-3 text-center">
                             <label class="form-label">Contraseña:</label>
-                            <input type="password" name="password" class="form-control">
+                            <input type="password" name="password" class="form-control" required>
                         </div>
                         <div class="mb-3 d-flex justify-content-center">
                             <button type="submit" class="btn btn-primary" name="login">Iniciar</button>
