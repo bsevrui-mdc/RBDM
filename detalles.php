f4762798722bab0fac1c21704e30ed990db8e6bd
MarianoRoldan47 <118228181+MarianoRoldan47@users.noreply.github.com>
2025-01-22 09:16:23 +0100

    Comentarios estructura

--- detalles.php
+++ detalles.php
@@ -9,34 +9,34 @@
 <body class="detalles">
     <?php include("includes/navbar.php"); ?>
     <main>
-        <div class="container transparente my-5 rounded-4">
+        <div class="container my-5 transparente rounded-4">
             <div class="row">
                 <div class="col-lg-3">
-                    <div class="row py-3">
+                    <div class="py-3 row">
                         <div class="col d-flex justify-content-center">
                             <img src="assets/img/Series/la_casa_de_papel.jpg" alt="imagenPelicula" class="img-fluid">
                         </div>
                     </div>
-                    <div class="row py-3">
-                        <div class="col text-center">
+                    <div class="py-3 row">
+                        <div class="text-center col">
                             <h1>La casa de papel</h1>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col text-center py-4">
-                            <div class="calificacion"><i class="fa-solid fa-star text-primary px-2"></i>9.83</div>
+                        <div class="py-4 text-center col">
+                            <div class="calificacion"><i class="px-2 fa-solid fa-star text-primary"></i>9.83</div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-9 px-5">
+                <div class="px-5 col-lg-9">
                     <div class="row">
-                        <div class="col text-start py-2 px-2">
+                        <div class="px-2 py-2 col text-start">
 
                             <h2>sinopsis</h2>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col py-3">
+                        <div class="py-3 col">
                             <p>
                                 Un misterioso hombre conocido como «el Profesor» ha pasado toda su vida planeando el mayor de los atracos de la historia: entrar en la Fábrica Nacional de Moneda y Timbre e imprimir 2400 millones de euros. Para llevar a cabo este ambicioso plan, el Profesor recluta a un equipo de ocho personas con ciertas habilidades y que no tienen nada que perder. Estos, junto al Profesor, planean cada paso del atraco durante cinco meses. Este equipo, con nombres de diferentes ciudades del mundo, requiere de 11 días de reclusión en la Fábrica, durante los cuales tiene que lidiar con las fuerzas de élite de la policía y 67 rehenes.
                             </p>
@@ -44,7 +44,7 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col py-3 px-2">
+                        <div class="px-2 py-3 col">
                             <h3>Fecha publicacion: 01-01-2020</h3>
                         </div>
                     </div>
@@ -75,10 +75,10 @@
                     </div>
                 </div>
             </div>
-            <div class="row my-5">
+            <div class="my-5 row">
                 <div class="col-lg-3 order-lg-1 d-flex align-items-center justify-content-center flex-column">
                     <h3>DIRECTOR</h3>
-                    <img src="./assets/img/reparto/Alex_Pina.jpg" alt="imagen" class="img-fluid py-2">
+                    <img src="./assets/img/reparto/Alex_Pina.jpg" alt="imagen" class="py-2 img-fluid">
                     <h1 class="pb-5 text-center">Alex Pina</h1>
                 </div>
                 <div class="col-lg-9">
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                     <div class="row align-items-stretch">
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card rounded-4 h-100">
                                 <img src="./assets/img/reparto/Alvaro_Morte.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -96,7 +96,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card rounded-4 h-100">
                                 <img src="./assets/img/reparto/Ursula_Corbero.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -104,7 +104,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card rounded-4 h-100">
                                 <img src="./assets/img/reparto/Pedro_Alonso.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -116,7 +116,7 @@
 
 
                     <div class="row">
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card rounded-4">
                                 <img src="./assets/img/reparto/Jaime_Lorente.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -124,7 +124,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card">
                                 <img src="./assets/img/reparto/Itziar_Ituño.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -132,7 +132,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 px-3 py-5">
+                        <div class="px-3 py-5 col-lg-4">
                             <div class="card">
                                 <img src="./assets/img/reparto/Miguel_Herran.jpg" class="img-fluid card-img-top rounded-4" alt="imagen">
                                 <div class="card-body">
@@ -144,6 +144,24 @@
                 </div>
 
             </div>
+        </div>
+        <div class="container my-5 transparente rounded-4 py3">
+            <div class="row">
+                <h2>comentarios</h2>
+            </div>
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col">
+                        <textarea class="rounded" rows="10" placeholder="Escribe un comentario aqui..."></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-end">
+                        <input type="submit" name="comentar" value="Comentar" class="btn btn-primary" />
+
+                    </div>
+                </div>
+            </form>
         </div>
     </main>
     <?php include("includes/footer.php"); ?>
