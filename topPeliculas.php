818293c4d6ca5490d55696f8e09820b1a15a67cc
RafaRoldan10 <roldancamposrafael@gmail.com>
2025-01-23 18:53:36 +0100

    changes on top Sries & Peliculas

--- topPeliculas.php
+++ topPeliculas.php
@@ -7,6 +7,26 @@
 </head>
 
 <body>
+    <?php
+    require_once 'funciones.php';
+    $contador = 1;
+    $conn = conectarConBBDD();
+
+    if (!isset($_SESSION['usuario'])) {
+        try {
+            $datosPelicula = $conn->query("SELECT c.id as id, c.nombre as nombre, c.nota as nota, c.genero as genero, c.imagen as imagen from contenido c where c.tipo = 'Pelicula' order by c.nota desc LIMIT 4");
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+    } else {
+        $id = $_SESSION["usuario"]->id;
+        try {
+            $datosPelicula = $conn->query("SELECT c.id as id, c.nombre as nombre, c.nota as nota, c.genero as genero, c.imagen as imagen, l.nota as notaUsuario from contenido c, lista l where c.tipo = 'Pelicula' and l.id_usuario='$id' and l.id_contenido = c.id order by c.nota desc LIMIT 4");
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+    }
+    ?>
     <?php include("includes/navbar.php"); ?>
     <main>
         <div class="container-fluid">
@@ -22,114 +42,40 @@
                 <div class="text-center col-lg-2 justify-content-center">Valoración</div>
                 <div class="text-center col-lg-2 justify-content-center">Tu nota</div>
             </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
-                    <div class="nota">1</div>
-                </div>
-                <div class="col-lg-2 contenedorImagen"><img src="./assets/img/peliculas/descarrilados.png" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="py-2 text-center col text-lg-start">
-                            <a href="detalles.php">Descarrilados</a>
+            <?php
+            while ($fila = $datosPelicula->fetchObject()) {
+            ?>
+                <div class="row top">
+                    <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
+                        <div class="nota"><?php echo $contador ?></div>
+                    </div>
+                    <div class="col-lg-2 contenedorImagen"><img src="<?php echo $fila->imagen ?>" alt="imagen" class="img-fluid w-100 h-100"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-lg-75">
+                            <div class="py-2 text-center col text-lg-start">
+                                <a href="detalles.php?peli=<?php echo $fila->id ?>"><?php echo $fila->nombre ?></a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="my-auto col-7 bordeGenero align-items-center">
+                                <h2 class="m-0"><?php echo $fila->genero ?></h2>
+                            </div>
+                            <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
+                                <div class="nota"><i class="fa-solid fa-star text-primary"></i><?php echo $fila->nota ?></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="my-auto col-7 bordeGenero align-items-center">
-                            <h2 class="m-0">COMEDIA</h2>
-                        </div>
-                        <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
+                        <div class="nota"><i class="fa-solid fa-star text-primary"></i><?php echo $fila->nota ?></div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
+                        <div class="nota"><?php if (isset($fila->notaUsuario)) echo $fila->notaUsuario ?></div>
                     </div>
                 </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
-                    <div class="nota">2</div>
-                </div>
-                <div class="col-lg-2 contenedorImagen"><img src="./assets/img/peliculas/interstellar.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="py-2 text-center col text-lg-start">
-                            <a href="detalles.php">Interstellar</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="my-auto col-7 bordeGenero align-items-center">
-                            <h2 class="m-0">ACCION</h2>
-                        </div>
-                        <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
-                    <div class="nota">3</div>
-                </div>
-                <div class="col-lg-2 contenedorImagen"><img src="./assets/img/peliculas/chucky.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="py-2 text-center col text-lg-start">
-                            <a href="detalles.php">Chucky</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="my-auto col-7 bordeGenero align-items-center">
-                            <h2 class="m-0">TERROR</h2>
-                        </div>
-                        <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
-                    <div class="nota">4</div>
-                </div>
-                <div class="col-lg-2 contenedorImagen"><img src="./assets/img/peliculas/padre.jpg" alt="imagen" class="img-fluid w-100 h-100"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="py-2 text-center col text-lg-start">
-                            <a href="detalles.php">Padre no hay más que uno</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="my-auto col-7 bordeGenero align-items-center">
-                            <h2 class="m-0">COMEDIA</h2>
-                        </div>
-                        <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
+            <?php
+                $contador++;
+            }
+            ?>
         </div>
     </main>
     <?php include("includes/footer.php"); ?>
--- topSeries.php
+++ topSeries.php
@@ -7,121 +7,75 @@
 </head>
 
 <body>
+    <?php
+    require_once 'funciones.php';
+    $contador = 1;
+    $conn = conectarConBBDD();
+
+    if (!isset($_SESSION['usuario'])) {
+        try {
+            $datosPelicula = $conn->query("SELECT c.id as id, c.nombre as nombre, c.nota as nota, c.genero as genero, c.imagen as imagen from contenido c where c.tipo = 'Serie' order by c.nota desc LIMIT 4");
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+    } else {
+        $id = $_SESSION["usuario"]->id;
+        try {
+            $datosPelicula = $conn->query("SELECT c.id as id, c.nombre as nombre, c.nota as nota, c.genero as genero, c.imagen as imagen, l.nota as notaUsuario from contenido c, lista l where c.tipo = 'Serie' and l.id_usuario='$id' and l.id_contenido = c.id order by c.nota desc LIMIT 4");
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+    }
+    ?>
     <?php include("includes/navbar.php"); ?>
     <main>
         <div class="container-fluid">
-            <div class="row my-4">
-                <div class="col text-center">
+            <div class="my-4 row">
+                <div class="text-center col">
                     <h1>Ranking de Series</h1>
                 </div>
             </div>
-            <div class="row my-4 d-none d-lg-block d-lg-flex">
-                <div class="col-lg-2 justify-content-center text-center">Ranking</div>
-                <div class="col-lg-2 justify-content-center text-center">Imagen</div>
-                <div class="col-lg-4 justify-content-center text-center">Titulo</div>
-                <div class="col-lg-2 justify-content-center text-center">Valoración</div>
-                <div class="col-lg-2 justify-content-center text-center">Tu nota</div>
+            <div class="my-4 row d-none d-lg-block d-lg-flex">
+                <div class="text-center col-lg-2 justify-content-center">Ranking</div>
+                <div class="text-center col-lg-2 justify-content-center">Imagen</div>
+                <div class="text-center col-lg-4 justify-content-center">Titulo</div>
+                <div class="text-center col-lg-2 justify-content-center">Valoración</div>
+                <div class="text-center col-lg-2 justify-content-center">Tu nota</div>
             </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex"><div class="nota">2</div></div>
-                <div class="col-lg-2 contenedorImagen"><img src="assets/img/Series/la_casa_de_papel.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="col text-center text-lg-start py-2">
-                            <a href="detalles.php">La casa de papel</a>
+            <?php
+            while ($fila = $datosPelicula->fetchObject()) {
+            ?>
+                <div class="row top">
+                    <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex">
+                        <div class="nota"><?php echo $contador ?></div>
+                    </div>
+                    <div class="col-lg-2 contenedorImagen"><img src="<?php echo $fila->imagen ?>" alt="imagen" class="img-fluid w-100 h-100"></div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="row d-flex align-items-center h-lg-75">
+                            <div class="py-2 text-center col text-lg-start">
+                                <a href="detalles.php?peli=<?php echo $fila->id ?>"><?php echo $fila->nombre ?></a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="my-auto col-7 bordeGenero align-items-center">
+                                <h2 class="m-0"><?php echo $fila->genero ?></h2>
+                            </div>
+                            <div class="my-auto col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center">
+                                <div class="nota"><i class="fa-solid fa-star text-primary"></i><?php echo $fila->nota ?></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-7 bordeGenero  align-items-center my-auto">
-                            <h2 class="m-0">ACCION</h2>
-                        </div>
-                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
+                        <div class="nota"><i class="fa-solid fa-star text-primary"></i><?php echo $fila->nota ?></div>
+                    </div>
+                    <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
+                        <div class="nota"><?php if (isset($fila->notaUsuario)) echo $fila->notaUsuario ?></div>
                     </div>
                 </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex"><div class="nota">2</div></div>
-                <div class="col-lg-2 contenedorImagen"><img src="assets/img/Series/breaking-bad.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="col text-center text-lg-start py-2">
-                            <a href="detalles.php">Breaking Bad</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-7 bordeGenero  align-items-center my-auto">
-                            <h2 class="m-0">COMEDIA</h2>
-                        </div>
-                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex"><div class="nota">2</div></div>
-                <div class="col-lg-2 contenedorImagen"><img src="assets/img/Series/from_netflix.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="col text-center text-lg-start py-2">
-                            <a href="detalles.php">FROM</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-7 bordeGenero  align-items-center my-auto">
-                            <h2 class="m-0">TERROR</h2>
-                        </div>
-                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
-            <div class="row top">
-                <div class="col-lg-2 align-items-center justify-content-center d-none d-lg-flex"><div class="nota">2</div></div>
-                <div class="col-lg-2 contenedorImagen"><img src="assets/img/Series/Lqsa.jpg" alt="imagen" class="img-fluid"></div>
-                <div class="col-lg-4 d-flex flex-column">
-                    <div class="row d-flex align-items-center h-lg-75">
-                        <div class="col text-center text-lg-start py-2">
-                            <a href="detalles.php">La que se avecina</a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-7 bordeGenero  align-items-center my-auto">
-                            <h2 class="m-0">COMEDIA</h2>
-                        </div>
-                        <div class="col-5 bordeNota d-flex justify-content-end d-lg-none align-items-center my-auto">
-                            <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota"><i class="fa-solid fa-star text-primary"></i>9.35</div>
-                </div>
-                <div class="col-lg-2 justify-content-center align-items-center border-start d-lg-flex d-none">
-                    <div class="nota">9.45</div>
-                </div>
-            </div>
+            <?php
+                $contador++;
+            }
+            ?>
         </div>
     </main>
     <?php include("includes/footer.php"); ?>
