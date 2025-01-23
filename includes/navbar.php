f61a53f0d91b9984fb2e0a8e43a1a15c6ac9a658
RafaRoldan10 <roldancamposrafael@gmail.com>
2025-01-23 17:19:30 +0100

    changes on navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,38 +10,50 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
         <i class="fa-solid fa-bars "></i>
       </button>
+      <div class="ms-3 dropdown">
+        <?php
+        if (!isset($_SESSION["usuario"]->imagen)) { ?>
+          <a href="#" class="text-white d-inline-block" data-bs-toggle="dropdown" aria-expanded="false"><i
+              class="fa-solid fa-user"></i></a>
+        <?php
+        } else {
+        ?>
+          <img src="<?php echo $_SESSION['usuario']->imagen ?>" data-bs-toggle="dropdown" class="rounded-circle">
+        <?php
+        }
+        ?>
+        <ul class="dropdown-menu bg-secondary dropdown-menu-end menuDropdown">
+          <?php
+          if (isset($_SESSION['usuario'])) {
+          ?>
+            <li><a class="dropdown-item" href="usuario.php">Perfil</a></li>
+            <?php
+            if ($_SESSION['usuario']->tipo == "admin") {
+            ?>
+              <li><a class="dropdown-item" href="admin.php">Administracion</a></li>
+            <?php
+            }
+            ?>
+            <hr>
+            <li><a class="dropdown-item" href="logout.php">Cerrar Sesión <i
+                  class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
+          <?php
+          } else {
+          ?>
+            <li><a class="dropdown-item" href="login.php">Iniciar Sesión</a></li>
+            <li><a class="dropdown-item" href="signup.php">Registrarse</a></li>
+          <?php
+          }
+          ?>
+        </ul>
+      </div>
     </div>
     <div class="collapse navbar-collapse" id="mynavbar">
       <ul class="navbar-nav me-auto">
         <li class="nav-item"><a class="nav-link" href="topPeliculas.php">Películas</a></li>
         <li class="nav-item"><a class="nav-link" href="topSeries.php">Series</a></li>
         <li class="nav-item"><a class="nav-link" href="eleccionGenero.php">Géneros</a></li>
-        <li class="d-lg-none nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="usuarioDropdown" data-bs-toggle="dropdown"
-            aria-expanded="false">Usuario</a>
-          <ul class="dropdown-menu bg-secondary dropdown-menu-end menuDropdown"
-            aria-labelledby="usuarioDropdown">
-            <?php
-            if (isset($_SESSION['usuario'])) {
-              if ($_SESSION['usuario']->tipo == 'admin') {
-            ?>
-                <li><a class="dropdown-item item" href="admin.php">Administracion</a></li>
-              <?php } ?>
-              <li><a class="dropdown-item item" href="usuario.php">Perfil</a></li>
-              <hr>
-              <li><a class="dropdown-item item" href="logout.php">Log Out <i
-                    class="fa-solid fa-arrow-right-from-bracket icono-Log-Out"></i></a></li>
-            <?php
 
-            } else {
-            ?>
-              <li><a class="dropdown-item item" href="login.php">Iniciar sesion</a></li>
-              <li><a class="dropdown-item item" href="login.php">Registrarse</a></li>
-
-            <?php } ?>
-
-          </ul>
-        </li>
       </ul>
     </div>
     <!-- Navbar >=lg -->
