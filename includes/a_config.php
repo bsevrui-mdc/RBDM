eb84b42ae7a1e545a3d0d42967f6ea5098e3f601
MarianoRoldan47 <118228181+MarianoRoldan47@users.noreply.github.com>
2025-01-23 10:05:55 +0100

    a_config.php

--- includes/a_config.php
+++ includes/a_config.php
@@ -53,3 +53,30 @@ switch ($_SERVER["SCRIPT_NAME"]) {
 		$PAGE_TITLE = "GCM - RBDM";
 		break;
 }
+
+
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId(getenv('GOOGLE_CLIENT_ID'));
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret(getenv('GOOGLE_CLIENT_SECRET'));
+
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri('http://' . $_SERVER['SERVER_NAME'] . '/index.php');
+
+
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+//start session on web page
+session_start();
+
+$login_button = '';
